4c4bbdb5b6125eed26dda6cd75399774e30378c4
may <hedjchaima@gmail.com>
2025-10-20 11:04:22 -0400

    added new js files

---
+++ nav.js
@@ -0,0 +1,25 @@
+function splitAtRoot(path) {
+    const url = new URL(path, location.origin);
+    const pathFromRoot = url.pathname;
+    document.write("<br>----> path from root: " + pathFromRoot);
+    return pathFromRoot;
+}
+
+function setNav(current_path) {
+    current_path = splitAtRoot(current_path);
+
+    fetch("nav.html")
+        .then(r => r.text())
+        .then(html => {
+            const navElement = document.getElementById("main-nav");
+            navElement.innerHTML = html;
+            const navLinks = navElement.querySelectorAll("a");
+            for (let child of navLinks) {
+                if (child instanceof HTMLAnchorElement) {
+                    if (splitAtRoot(child.href) === current_path) {
+                        child.classList.add("current_page");
+                    }
+                }
+            }
+        });
+}
